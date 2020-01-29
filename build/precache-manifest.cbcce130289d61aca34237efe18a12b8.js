c438ec9111d993e8042625530991e29e3a70136d
Howard Yeend <h.yeend@growinginteractive.com>
2020-01-29 13:47:49 +0000

    github pages setup

---
+++ build/precache-manifest.cbcce130289d61aca34237efe18a12b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc80563cb85fdc4b3e717d6d52b9a7a",
+    "url": "/barbell-chooser/index.html"
+  },
+  {
+    "revision": "efc39260356b275961c4",
+    "url": "/barbell-chooser/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9de465409d7ff48b641f",
+    "url": "/barbell-chooser/static/css/main.3413440b.chunk.css"
+  },
+  {
+    "revision": "efc39260356b275961c4",
+    "url": "/barbell-chooser/static/js/2.a6ac1fb2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/barbell-chooser/static/js/2.a6ac1fb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9de465409d7ff48b641f",
+    "url": "/barbell-chooser/static/js/main.490c546f.chunk.js"
+  },
+  {
+    "revision": "4fcc446636e426c193e1",
+    "url": "/barbell-chooser/static/js/runtime-main.c711d86a.js"
+  }
+]);
